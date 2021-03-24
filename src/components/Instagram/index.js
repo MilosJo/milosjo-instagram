bb1eeb561a26ec9c57583f2f698639e4a48760a1
MilosJo <jovancevicmilos@gmail.com>
2021-03-24 19:30:58 +0100

    fix

--- src/components/Instagram/index.js
+++ src/components/Instagram/index.js
@@ -6,39 +6,21 @@ import './instagram.css';
 export default class Instagram extends React.Component {
   state = { photos: [], loading: true };
 
-  // Your specifications
-  INSTAGRAM_ID = '7351580318';
-
-  THUMBNAIL_WIDTH = 640;
-
-  PHOTO_COUNT = 12;
-
   async componentDidMount() {
     try {
       // Hack from https://stackoverflow.com/a/47243409/2217533
       const response = await fetch(
-        `https://graph.instagram.com/me/media?fields=media_url,permalink,username&access_token=IGQVJWV2NiUGNWeUw4a3NPSXdrRXFqZAUh2M3pjLUVEQnJNODhRNG1TcVZAvdFdBZAjN0Y2hhQVJTMU5QODYyRS1CcGI3NTJmd0d2TFA1UEFKTG1PRlp2RW1oT0k4WU5Ybmp0UUc5RVUzZA1dYLUpfRGtQRwZDZD`
+        `https://graph.instagram.com/me/media?fields=media_url,permalink,username,thumbnail_url&access_token=IGQVJWV2NiUGNWeUw4a3NPSXdrRXFqZAUh2M3pjLUVEQnJNODhRNG1TcVZAvdFdBZAjN0Y2hhQVJTMU5QODYyRS1CcGI3NTJmd0d2TFA1UEFKTG1PRlp2RW1oT0k4WU5Ybmp0UUc5RVUzZA1dYLUpfRGtQRwZDZD`
       );
       const { data } = await response.json();
-      const photos = data.user.edge_owner_to_timeline_media.edges.map(({ node }) => {
-        const { id } = node;
-        const comments = node.edge_media_to_comment.count;
-        const likes = node.edge_media_preview_like.count;
-        const caption = node.edge_media_to_caption.edges[0].node.text;
-        const thumbnail = node.thumbnail_resources.find(
-          (thumbnail) => thumbnail.config_width === this.THUMBNAIL_WIDTH
-        );
-        const { src, config_width: width, config_height: height } = thumbnail;
-        const url = `https://www.instagram.com/p/${node.shortcode}`;
+      console.log('data:', data);
+      const photos = data.map((photo) => {
+        const { id, media_url, username, permalink } = photo;
         return {
           id,
-          caption,
-          src,
-          width,
-          height,
-          url,
-          comments,
-          likes,
+          media_url,
+          username,
+          permalink,
         };
       });
       this.setState({ photos, loading: false });
@@ -48,17 +30,25 @@ export default class Instagram extends React.Component {
   }
 
   render() {
+    const { loading, photos } = this.state;
+
     return (
       <div className="post-wrapper">
-        {this.state.loading === true ? (
+        {loading === true ? (
           <div style={{ textAlign: 'center' }}>
             <h1>Loading ...</h1>
           </div>
         ) : (
-          this.state.photos &&
-          this.state.photos.map(({ src, url, id, likes, comments, caption }) => (
-            <a href={url} target="_blank" className="post-item" rel="noopener noreferrer" key={id}>
-              <img src={src} className="post-image" alt={caption.substring(0, 40)} />
+          photos &&
+          photos.map(({ id, media_url, username, permalink }) => (
+            <a
+              href={permalink}
+              target="_blank"
+              className="post-item"
+              rel="noopener noreferrer"
+              key={id}
+            >
+              <img src={media_url} className="post-image" alt={username} />
               {/*  */}
               <div className="post-item-info">
                 <ul>
@@ -77,7 +67,6 @@ export default class Instagram extends React.Component {
                         <path d="M12 4.435C10.011-.964 0-.162 0 8.003 0 12.071 3.06 17.484 12 23c8.94-5.516 12-10.929 12-14.997C24-.115 14-.996 12 4.435z" />
                       </svg>
                     </span>{' '}
-                    {likes !== null ? likes.toLocaleString() : 0}
                   </li>
                   <li className="post-item-comments">
                     <span role="img" aria-label="speech-balloon">
@@ -94,7 +83,6 @@ export default class Instagram extends React.Component {
                         <path d="M24 9.874C24 4.42 18.627 0 12 0S0 4.42 0 9.874c0 4.512 3.678 8.317 8.701 9.496L12 24l3.299-4.63C20.322 18.19 24 14.385 24 9.874z" />
                       </svg>
                     </span>{' '}
-                    {comments !== null ? comments.toLocaleString() : 0}
                   </li>
                 </ul>
               </div>
